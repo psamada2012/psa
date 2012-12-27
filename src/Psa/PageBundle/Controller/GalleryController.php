628adb2553e3ef03592ea02d0d4e1f3c9a62d7c4
ketrika <gasysoft@gmail.com>
2012-12-27 17:41:50 +0300

    menu admin albums

--- src/Psa/PageBundle/Controller/GalleryController.php
+++ src/Psa/PageBundle/Controller/GalleryController.php
@@ -13,10 +13,14 @@ class GalleryController extends Controller
         $em = $this->getDoctrine()->getEntityManager();
         if($album_id == null){
             $entities = $em->getRepository('PsaAdminBundle:Gallery')->findAll();
+            $album = null;
         }else{
             $entities = $em->getRepository('PsaAdminBundle:Gallery')->findBy(array('album_id'=>$album_id));
+            $album = $em->getRepository('PsaAdminBundle:Albums')->find($album_id);
         }
+        
         $aParam['galleries'] = $entities;
+        $aParam['album'] = $album;
         
         return $this->render('PsaPageBundle:Gallery:index.html.twig',$aParam);
     }
